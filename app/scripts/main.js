28f262c2488482a9ef4b7c78d0fac9b24a34e99a
Neil Pullman <npullman@thisisgrow.com>
2016-12-01 14:09:14 -0500

    add in-browser visuals to test collision areas

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,11 +5,14 @@ let width = document.documentElement.clientWidth,
     height = document.documentElement.clientHeight,
     canvas = document.createElement('canvas'),
     gl = canvas.getContext('webgl', { preserveDrawingBuffer: true }),
+    canvas2d = document.createElement('canvas'),
+    ctx = canvas2d.getContext('2d'),
     mouseX = 0.0,
     mouseY = 0.0,
     isMouseDown = false,
     progressBar,
-    fbo = null
+    fbo = null,
+    pixelMap = null;
 
 
 // loader's self-calling. it calls itself.
@@ -18,8 +21,9 @@ var loader = new Loader({
     document.getElementById('load_progress').classList.add('done');
     document.querySelector('section').style.opacity = 1;
 
-    canvas.width = tree.width;
-    canvas.height = tree.height;
+    canvas.width = canvas2d.width = tree.width;
+    canvas.height = canvas2d.height = tree.height;
+
 
     const regl = require('regl')({
       gl: gl,
@@ -35,9 +39,7 @@ var loader = new Loader({
       colorType: 'float'
     });
 
-    document.querySelector('#renderer_container').appendChild(canvas)
-
-    const drawCanvas = regl({
+    const buildPixelMap = regl({
       frag: glslify('./app/scripts/shader.frag'),
       vert: glslify('./app/scripts/shader.vert'),
       count: 3,
@@ -53,20 +55,46 @@ var loader = new Loader({
       uniforms: { texture: regl.prop('texture') }
     })
 
-    var time = new Date().getTime();
+    // calculate start time so we can test pixelmap generation times
+    var startTime = new Date().getTime();
 
-    drawCanvas({
+    buildPixelMap({
       texture: regl.texture(tree)
     });
 
-    var pixels = regl.read();
+    pixelMap = regl.read();
+
+    console.log('read in ' + (new Date().getTime() - startTime) / 1000 + ' seconds')
+
+
+    //
+    //  draw canvas to screen to visualize hit areas
+    //
+    canvas2d.addEventListener('mousemove', (e) => {
+      // if hit, change cursor to pointer. if not, switch back to default
+      if (pixelMap[pixelMap.length - (e.offsetY *  tree.width + e.offsetX) * 4] == 255) {
+        document.body.style.cursor = 'pointer';
+      } else {
+        document.body.style.cursor = 'default';
+      }
+    })
+
+    // var imageData = ctx.getImageData(0, 0, canvas2d.width, canvas2d.height);
+    // var data = imageData.data;
+
+    // for (var i = 0, d = data.length; i < d; i++) {
+    //   data[i] = pixelMap[i];
+    // }
+
+    ctx.drawImage(tree, 0, 0);
+
+    // ctx.putImageData(imageData, 0, 0);
+    document.querySelector('#renderer_container').appendChild(canvas2d);
 
     // for (var i = 0, p = pixels.length; i < p; i += 4) {
     //   if (pixels[i] == 255) {
     //      console.log(i / 4, i / 4 % tree.width, Math.floor(i / 4 / tree.width));
     //   }
     // }
-
-    console.log('read in ' + (new Date().getTime() - time) / 1000 + 'seconds')
   }
 });
--- app/scripts/shader.vert
+++ app/scripts/shader.vert
@@ -6,5 +6,5 @@ varying vec2 vTextureCoord;
 void main () {
   vTextureCoord = position;
 
-  gl_Position = vec4(1.0 - 2.0 * position, 0, 1.0) * vec4(-1.0, 1.0, 1.0, 1.0);
+  gl_Position = vec4(1.0 - 2.0 * position, 0, 1.0);
 }
